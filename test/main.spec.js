3ff802741e8f5580f3f35e07428738fc01bd5e06
rmoskal <rmoskal@mostmedioa.com>
2016-06-08 11:32:03 -0400

    dots don't work in export statements

--- test/main.spec.js
+++ test/main.spec.js
@@ -9,32 +9,32 @@ describe ("meteor-settings-from-env", function(){
 
     it('pulls out the configs with the default prefix', function(){
         var toTest = require( '../lib/main')();
-        var res = toTest._extractKeys({"METEOR.one":"one", two:'two', "METEOR.three":'three'}, {});
+        var res = toTest._extractKeys({"METEOR_one":"one", two:'two', "METEOR_three":'three'}, {});
         res.should.deep.equal({"one":"one", "three":'three'});
     });
 
     it('pulls out the configs with a different prefix', function(){
         var toTest = require( '../lib/main')('FOO.');
-        var res = toTest._extractKeys({"FOO.one":"one", two:'two', "METEOR.three":'three'}, {});
+        var res = toTest._extractKeys({"FOO.one":"one", two:'two', "METEOR_three":'three'}, {});
         res.should.deep.equal({"one":"one"});
     });
 
 
     it("merges all the keys starting with 'public' ", function(){
         var toTest = require( '../lib/main')();
-        var res = toTest._remapPublicKeys({"one":"one", two:'two', "public.three":'three'});
+        var res = toTest._remapPublicKeys({"one":"one", two:'two', "PUBLIC_three":'three'});
         res.should.deep.equal({"one":"one", two:'two', "public" :{three:'three'}});
     });
 
     it("handles the edge case of all public 'public' ", function(){
         var toTest = require( '../lib/main')();
-        var res = toTest._remapPublicKeys({"public.one":"one", "public.two":'two', "public.three":'three'});
+        var res = toTest._remapPublicKeys({"PUBLIC_one":"one", "PUBLIC_two":'two', "PUBLIC_three":'three'});
         res.should.deep.equal({"public" :{"one":"one", two:'two',three:'three'}});
     });
 
     it("does the full job", function(){
         var toTest = require( '../lib/main')();
-        process.env["METEOR.one"] = "test";
+        process.env["METEOR_one"] = "test";
         var settings = {one:'hey'};
         var res = toTest.do(settings);
         res.should.deep.equal({"one":"test"});
@@ -42,7 +42,7 @@ describe ("meteor-settings-from-env", function(){
 
     it("does the full job with public variables", function(){
         var toTest = require( '../lib/main')();
-        process.env["METEOR.public.one"] = "test";
+        process.env["METEOR_PUBLIC_one"] = "test";
         var settings = {one:'hey'};
         var res = toTest.do(settings);
         res.should.deep.equal({ one: 'test', public: { one: 'test' } });
